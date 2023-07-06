3ae8b0a478e5125a78d29ecfae8751c8baee818f
Avery Miller <averymiller2060@gmail.com>
2023-07-05 23:35:32 -0400

    added functional addition operator

--- assets/logic/script.js
+++ assets/logic/script.js
@@ -3,23 +3,52 @@ const calculate = document.querySelector("[value=equal]");
 const buttons = document.querySelector(".buttons");
 const equation = document.querySelector(".equation");
 
-buttons.addEventListener("click", function(e) {
+function buttonHandler(e) {
   const btn = e.target;
   let value = btn.value;
   if(btn.classList.contains("numeric")) {
-    if(value === "decimal") {
-      value = ".";
-    }
-    input.value += value;
+    numericHandler(value);
   } else if(btn.classList.contains("operator")) {
-    equation.innerHTML += `${input.value} ${value} `;
-    input.value = "";
+    operatorHandler(value);
+  } else if(btn.value === "equal") {
+    equalHandler();
   }
-});
+}
 
-calculate.addEventListener("click", function() {
-  console.log(input.value);
-});
+let solution;
+let input1;
+let input2;
+let op;
+
+function numericHandler(value) {
+  input.value += value;
+} 
+
+function operatorHandler(value) {
+  input1 = parseInt(input.value);
+  op = value;
+
+  equation.innerHTML += `${input.value} ${value} `;
+  input.value = "";
+}
+
+function equalHandler() {
+  equation.innerHTML = `${input1} ${op} ${input.value} =`;
+
+  input2 = parseInt(input.value);
+  switch(op) {
+    case '+':
+      solution = input1 + input2;
+      break;
+  }
+  input.value = solution;
+}
+
+buttons.addEventListener("click", buttonHandler);
+
+// calculate.addEventListener("click", function() {
+//   console.log(input.value);
+// });
 
 window.addEventListener("DOMContentLoaded", function() {
   input.focus();
